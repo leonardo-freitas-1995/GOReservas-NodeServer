2fd1a13dfdbd9be192a50055cdc294117467435a
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-12-07 00:08:07 -0200

    Arrumando erro do teste loginFail

--- test/e2e/loginFail-spec.js
+++ test/e2e/loginFail-spec.js
@@ -1,14 +1,23 @@
+var request = require('request');
 
 describe('Página de Login do Go Reservas', function(){
+    beforeEach(function() {
+        request.post("http://localhost:3030/api/test/createTestUser");
+    });
 
 	it('Não deve logar na página inicial', function(){
 		browser.get('http://localhost:3030/index');
+        element(by.css('[href="#loginModal"]')).click();
 		element(by.model('vm.account.email')).sendKeys('teste@teste');
 		element(by.model('vm.account.password')).sendKeys('wrongpassword');
-		element(by.bycss('[ng-click="vm.login()"]')).click();
+		element(by.css('[ng-click="vm.login()"]')).click();
 
 		expect(browser.getCurrentUrl())
 		.toBe('http://localhost:3030/index');
 	});
+
+    afterEach(function() {
+        request.post("http://localhost:3030/api/test/cleanTestUser");
+    });
 
 });
