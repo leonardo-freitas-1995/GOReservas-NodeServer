18692dd3ff444470c42169823beae4212c6d260f
Leonardo Freitas dos Santos <leonardo_freitas1995@hotmail.com>
2016-12-07 22:57:38 -0200

    Criando teste para criação de reserva com erro

---
+++ test/e2e/createReserveFail-spec.js
@@ -0,0 +1,44 @@
+var request = require('request');
+var settings = require("../protractor-settings");
+var Application = require('../pages.js');
+
+describe('Página de Estabelecimento do GO Reservas', function(){
+	beforeEach(function() {
+		request.post(settings.host + settings.testAPI.addUser);
+		request.post(settings.host + settings.testAPI.addBusiness);
+	});
+
+	it('Deve realizar uma reserva', function(){
+		var application = new Application();
+		var businessPage = application.login().withCredentials(settings.testUser.email, settings.testUser.password)
+			.goToBusinessPage(settings.testBusiness.id);
+
+        browser.executeScript("window.scrollTo(0,0);").then(function () {
+            element(by.css('[ng-href="#reserveModal"]')).click();
+            browser.executeScript("$('input.display-none').removeClass('display-none');").then(function () {
+                element(by.model('vm.newReserve.day')).sendKeys(settings.testReserveNotUsed.day);
+                element(by.model('vm.newReserve.hour')).sendKeys(settings.testReserveNotUsed.hour);
+                element(by.model('vm.newReserve.quantity')).sendKeys(settings.testBusiness.maxPerReserve + 1);
+                element(by.model('vm.newReserve.observation')).sendKeys(settings.testReserveNotUsed.observation);
+                element(by.css('[ng-click="vm.createReserve()"]')).click();
+                element(by.css('.modal-close')).click();
+                businessPage.backToDashboard();
+				browser.wait(protractor.ExpectedConditions.stalenessOf ($('.lean-overlay')), 10000);
+                expect(element(by.css('[name="reserveCard"]')).isPresent())
+                    .toBe(false);
+            })
+
+
+
+        })
+
+
+	});
+
+	afterEach(function() {
+		request.post(settings.host + settings.testAPI.removeUser);
+		request.post(settings.host + settings.testAPI.removeBusiness);
+		request.post(settings.host + settings.testAPI.removeReserve);
+	});
+
+});
